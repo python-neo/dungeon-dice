91046f9775f6e36adf4a8e8be1c8d0cf247040fc
Aarav Agarwal <279409447+python-neo@users.noreply.github.com>
2026-02-19 12:31:24 +0530

    Added player.py and basic commands

--- main/main.py
+++ main/main.py
@@ -1,27 +1,22 @@
-from rich.console import Console
+from .console import console
 from rich.panel import Panel
 from rich.prompt import Prompt
-from random import randint
+from .player import Player
 
-console = Console ()
+player = Player ()
 
-class Player :
-    def __init__ (self) :
-        self.hp = 100
-        self.gold = 0
-        self.potions = {"Healing Potion" : 0,
-                        "Strength Potion" : 0,
-                        "Escape Potion" : 0,
-                        "Greater Healing" : 0}
+console.print (Panel ("Welcome to Dungeon Dice!", title = "Dungeon Dice 🎲"))
 
-    def enter_room (self) :
-        roll = Dice.roll ()
-        if roll == 1 :
-            console.print ("You find an empty room...")
-
-class Dice :
-    @staticmethod
-    def roll () :
-        return randint (1, 4)
-
-console.print ()
+while True :
+    command = Prompt.ask ("> ")
+    commands = command.strip ().lower ().split (" ")
+    try :
+        func = player.commands [commands [0]]
+        console.clear ()
+    except KeyError :
+        console.print ("[bold red]Command not found.[/]")
+        continue
+    if func ["args"] :
+        func ["command"] (*commands [1:])
+    else :
+        func ["command"] ()
